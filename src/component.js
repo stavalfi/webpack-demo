043fea96e2b85398adee59475d7b291f67314c20
stav alfi <stav.alfi@nice.com>
2018-08-28 08:16:36 +0300

    * removed typescript support * added babel support and babel loaders in latest versions (7.0.0 for babel, 8.0.0 for babel-loader)

--- src/component.js
+++ src/component.js
@@ -1,7 +1,17 @@
-export default (text = "1") => {
+Array.apply(null, {length: 10})
+    .map(Function.call, Math.random)
+    .forEach(console.log)
+
+Promise.resolve().finally();
+class StavAlfi{
+
+}
+
+export default (text = "11112221") => {
     const element = document.createElement("div");
 
     element.innerHTML = text;
-
+    new StavAlfi();
     return element;
 };
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    mode: "development",
+    mode: process.env.NODE_ENV === 'development' ? "development" : "production",
     entry: "./src/index.js",
     output: {
+        // default output directory: "dist" under the main folder.
         filename: "bundle.js"
     },
     resolve: {
@@ -14,45 +15,43 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(jpg|jpeg)$/,
+                use: {
+                    // will accept imports of the above files (usually images) and if they are equal/less then the option-limit I specified, then those files
+                    // will transform to strings and will be added to the JS bundle. If they are bigger then this loader will invoke file-loader (if no fall-back is defined) which will
+                    // copy the file as it is a specified location.
+                    // it is for making the browser to make less calls to different files other the js bundle file.
+                    loader: "url-loader",
+                    options: {
+                        // it is for preventing the js bundle file to be too big and then the browser will need much time to load it.
+                        limit: 25000,
+                    },
+                },
+            },
+            {
+                test: /\.(png)$/,
+                use: {
+                    // will copy the file to a specified location under the output directory I specified and I need to find this file from code in there (when using imports).
+                    // requirement: every file main-project-folder/X/Y/Z.abc needs to be in output-dir/X/Y/Z.abc and in the code I need to locate, relatively the this location.
+                    loader: "file-loader",
+                    options: {
+                        name: "[path][name].[hash].[ext]",
+                    },
+                },
+            },
+            {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                exclude: [
-                    "node_modules",
-                    "webpack.config.js",
-                    "dist"
-                ]
-            },
-            {
-                test: /\.ts$/,
-                enforce: 'pre',
-                use: [
-                    {
-                        loader: 'tslint-loader',
-                        options: {
-                            // tslint errors are displayed by default as warnings
-                            // set emitErrors to true to display them as errors
-                            emitErrors: true,
-                            // tslint does not interrupt the compilation by default
-                            // if you want any file with tslint errors to fail
-                            // set failOnHint to true
-                            failOnHint: true,
-                            // enables type checked rules like 'for-in-array'
-                            // uses tsconfig.json from current working directory
-                            typeCheck: true,
-                            // automatically fix linting errors
-                            fix: true,
-                        }
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: true
                     }
-                ],
-                exclude: [
-                    "node_modules",
-                    "webpack.config.js",
-                    "dist"
-                ]
+                }
             }
         ]
     },
@@ -63,11 +62,13 @@ module.exports = {
         }),
         // put all the css files in one css file and not in the js files because js files can take time until they load so mean while the css are not loaded.
         // in this way, the browser can manage the process by him self because css and js are in different files.
+        // it's currently not emitting css files - bug in their plugin.
         new MiniCssExtractPlugin({
             // [name] will be the name of the entry file which defined in the webpack.config.js
             filename: "[name].css",
         })
     ],
+    // the dev server doesn't save any files in FS. he use in-memory FS because it is faster. so I won't find any actual bundled files in my actual FS.
     devServer: {
         overlay: true, // capturing compilation related warnings and errors and show them instead of showing my actual website.
         stats: "errors-only",
