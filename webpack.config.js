c3b37ccc1f0dacadf1278823858555729d07821f
stavalfi <stavalfi@gmail.com>
2018-08-29 23:02:09 +0300

    Configure hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,12 @@ const cssnano = require("cssnano");
 const safeParser = require('postcss-safe-parser');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+const isDevelopmentMode = process.env.NODE_ENV === 'development';
 module.exports = {
     //devtool: "source-map",
     // in production mode, the webpack does uglify and Scope Hoisting which means that all the modules are under
     // the same scope and not in different scopes. it's slow down the build but makes the code run faster.
-    mode: process.env.NODE_ENV === 'development' ? "development" : "production",
+    mode: isDevelopmentMode ? "development" : "production",
     entry: path.join(__dirname, "src", "index.js"),
     output: {
         // default output directory: "dist" under the main folder.
@@ -23,10 +24,10 @@ module.exports = {
         // each final output file is called chunk.
         // all the output files are called bundles and chunks and also bundles contain one or more chunks.
         // ....................................................................
-        // [name] == the name of the bundle that will be created. dependes if I use optimizations + there will be a cundle for each entry I list in webpack config file.
-        // [chunkhash] == a hash code that is generated from the content of the chunk
-        filename: "[name].[chunkhash].bundle.js",
-        path: path.join(__dirname, "src", "dist.js")
+        // [name] == the name of the chunk
+        // [chunkhash] == a hash code that is generated from the content (without metadata of webpack) of the chunk.
+        filename: "[chunkhash].bundle.js",
+        path: path.join(__dirname, "dist")
     },
     module: {
         rules: [
@@ -57,7 +58,10 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                use: [
+                    isDevelopmentMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ],
             },
             {
                 test: /\.js$/,
@@ -80,8 +84,7 @@ module.exports = {
         // in this way, the browser can manage the process by him self because css and js are in different files.
         // it's currently not emitting css files - bug in their plugin.
         new MiniCssExtractPlugin({
-            // [name] will be the name of the entry file which defined in the webpack.config.js
-            filename: "[name].css",
+            filename: "[chunkhash].css",
         }),
         // remove the dist folder before every webpack build.
         new CleanWebpackPlugin([path.join(__dirname, "dist")]),
@@ -105,10 +108,12 @@ module.exports = {
         // generate a visualize analyzer of the output bundles using a server.
         new BundleAnalyzerPlugin({
             analyzerPort: 8887
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ],
     // the dev server doesn't save any files in FS. he use in-memory FS because it is faster. so I won't find any actual bundled files in my actual FS.
     devServer: {
+        hot: true,
         overlay: true, // capturing compilation related warnings and errors and show them instead of showing my actual website.
         stats: "errors-only",
         host: process.env.HOST,
@@ -118,19 +123,38 @@ module.exports = {
     // control the bundle size.
     // it minimize using UglifyWebpackPlugin plugin by default.
     optimization: {
+        // will remove all webpack metadata from all chunks and extract them to one single chunk.
+        // if not, the content of each chunk will change because of this even if we didn't change anything in the code or in the npm modules
+        // .
+        // then the hash will change and then the user will have to download some of the chunks again.
+        runtimeChunk: 'single',
         // by default if we don't specify anything, the source code of every lib I'm importing in my JS code, will be added to the final bundle file.
         // the advantage is less calls to other files when the browser runs the code but the disadvantage is that the file is too big to cache it.
         // the solution is to split my code from the libs code I'm using. the advantage is that my code can be cached and the disadvantage that
         // I Will have a lot of calls to other files which contain the libs code. The libs must be in node_modules.
         // //  In production mode, there is a split by default.
         splitChunks: {
-            // by default, webpack 4 only split the bundel to 2 chunks: vendor (my dependencies from node_modules) and my actual source of of my app
-            // only for vendors that I requeire by async. because this is not supported in JS spec yet then I will activate this optimization to the
+            // by default, webpack 4 only split the bundle to 2 chunks: vendor (my dependencies from node_modules) and my actual source of of my app
+            // only for vendors that I require by async. because this is not supported in JS spec yet then I will activate this optimization to the
             // modules I require sync. which is all the modules.
             // initial refer to the vendors I require from the entry files listed in webpack config file not async (sync).
             // all == async + sync
             // async == only async.
-            chunks: "initial"
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
         }
     }
 };
+
+/*
+Additional notes:
+1. hash is calculated for a build, chunkhash is calculated for chunk (entry file), contenthash
+    is special hash generated in ExtractTextPlugin and is calculated by extracted content, not by
+    whole chunk content. See this article https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.f9aon6s3b
+2.
+ */
