fa52d2e6c1068da817406f37bc5be7b7c285c9bb
stavalfi <stavalfi@gmail.com>
2018-08-30 16:32:11 +0300

    implementing lodash operations

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,31 @@ const React = require("react");
 const ReactDOM = require("react-dom");
 
 export default class Welcome extends React.Component {
-  render() {
-    return <div>
-      Hello, <Name name={Math.random()}/>1112
-      </div>;
+  chunk(array, amount) {
+    return new Array(null, {
+        length: Math.ceil(array.length / amount)
+      })
+      .map(Number.call, Number)
+      .map(i => array.slice(i * amount, (i + 1) * amount));
   }
+  concat(array) {
+    return Object.keys(arguments)
+      .slice(1)
+      .reduce((result, param) => {
+        if (Array.isArray(param))
+          return result.concat(...param);
+        return result.concat([param]);
+      }, array);
+  }
+  difference(array, exclude) {
+    const map = exclude.reduce((result, element) => ({
+      ...result,
+      [element]: element
+    }), {});
+
+    return array.filter(element=> element in map);
+  }
+  render() {
+    return this.difference([1, 2, 3, 3], [1,3]);
+  };
 }
