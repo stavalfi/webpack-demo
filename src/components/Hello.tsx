c1abb2d8288893f20fb457d401942f6c66703cc3
stavalfi <stavalfi@gmail.com>
2018-09-02 10:18:29 +0300

    updated README.md for instructions

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export class Hello extends React.Component<{}, {}> {
     render() {
-        return <h1>put all your html css here!</h1>;
+        return <div id="your-dom">put all your html css here!</div>;
     }
 }
